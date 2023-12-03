1a313279d29584ea6536df0cfb40073fab679490
Saurav Savarn <sauravsavarn@gmail.com>
2023-12-03 21:43:28 +0530

    Changes: 1. Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+// App component represents the view which when launch can be seen in the browser.
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
